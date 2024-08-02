e944781644b8b5526ca0b9bb15ede8083d628393
ienisata <84399791+ienisata@users.noreply.github.com>
2024-08-02 16:57:53 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:havadurumu/screens/home_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
   runApp(const Home());
@@ -14,4 +15,9 @@ class Home extends StatelessWidget {
       home: HomePage(),
     );
   }
+}
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp());
 }
